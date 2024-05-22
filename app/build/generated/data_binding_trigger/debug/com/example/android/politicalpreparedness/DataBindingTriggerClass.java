9e5cc0b7ff3e4693e157fbeaf930c79c165f0b23
H Ngg <hnn@gmail.com>
2024-05-23 02:18:55 +0700

    Update .gitignore

--- app/build/generated/data_binding_trigger/debug/com/example/android/politicalpreparedness/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.example.android.politicalpreparedness;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- app/build/tmp/kapt3/stubs/debug/com/example/android/politicalpreparedness/database/ElectionDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.politicalpreparedness.database;
-
-import java.lang.System;
-
-@androidx.room.Dao
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\n\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\bg\u0018\u00002\u00020\u0001\u00a8\u0006\u0002"}, d2 = {"Lcom/example/android/politicalpreparedness/database/ElectionDao;", "", "app_debug"})
-public abstract interface ElectionDao {
-}
