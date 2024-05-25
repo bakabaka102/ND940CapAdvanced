f104fef4829aa929d02c2237d2615c55e8e3a873
H Ngg <hnn@gmail.com>
2024-05-25 16:28:44 +0700

    - Implement part 2: Update secure hide api key

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -2,28 +2,18 @@ package com.example.android.politicalpreparedness.network
 
 import okhttp3.OkHttpClient
 
-class CivicsHttpClient: OkHttpClient() {
+class CivicsHttpClient : OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
-
         fun getClient(): OkHttpClient {
-            return Builder()
-                    .addInterceptor { chain ->
-                        val original = chain.request()
-                        val url = original
-                                .url()
-                                .newBuilder()
-                                .addQueryParameter("key", API_KEY)
-                                .build()
-                        val request = original
-                                .newBuilder()
-                                .url(url)
-                                .build()
-                        chain.proceed(request)
-                    }
-                    .build()
+            return Builder().addInterceptor { chain ->
+                val original = chain.request()
+                val url = original.url().newBuilder()
+                    .addQueryParameter("key", APIConstants.API_KEY).build()
+                val request = original.newBuilder().url(url).build()
+                chain.proceed(request)
+            }.build()
         }
 
     }
